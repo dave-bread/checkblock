30ed7a4fedeef62170964137a0900ddaddf8c68e
suzuki.t <suzuki.t@arara.com>
2018-11-24 00:14:52 +0900

    commit

--- setup.py
+++ setup.py
@@ -12,13 +12,13 @@ with open('LICENSE') as f:
     license = f.read()
 
 setup(
-    name='sample',
+    name='checkblock',
     version='0.1.0',
-    description='Sample package for Python-Guide.org',
+    description='Mail block check',
     long_description=readme,
     author='Kenneth Reitz',
     author_email='me@kennethreitz.com',
-    url='https://github.com/kennethreitz/samplemod',
+    url='https://github.com/dave-bread/checkblock.git',
     license=license,
     packages=find_packages(exclude=('tests', 'docs'))
 )
